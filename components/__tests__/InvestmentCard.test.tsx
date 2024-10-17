64a76f8a4f8d51adcf562dcb73fded56650629c5
Eduardo Felizardo Cândido <153370257+dufelizardo@users.noreply.github.com>
2024-10-16 23:01:30 -0300

    primeiro commit

--- components/__tests__/InvestmentCard.test.tsx
+++ components/__tests__/InvestmentCard.test.tsx
@@ -9,10 +9,20 @@ jest.spyOn(Alert, 'alert');
 describe('InvestmentCard', () => {
   it('displays the correct investment information', () => {
     const { getByText } = render(
-      <InvestmentCard code="MXRF11" earnings="Rendimento: R$ 0,09" amount="R$ 11,52" date="DAQUI A 2 DIAS" />
+      <>
+        <InvestmentCard code="MXRF11" earnings="Rendimento: R$ 0,09" amount="R$ 11,52" date="DAQUI A 2 DIAS" />
+        <InvestmentCard code="VISC11" earnings="Rendimento: R$ 0,80" amount="R$ 10,40" date="DAQUI A 2 DIAS" />
+        <InvestmentCard code="RVBI11" earnings="Rendimento: R$ 0,75" amount="R$ 18,75" date="DAQUI A 3 DIAS" />
+        <InvestmentCard code="CPTS11" earnings="Rendimento: R$ 0,07" amount="R$ 2,95" date="DAQUI A 5 DIAS" />
+        <InvestmentCard code="MCHY11" earnings="Rendimento: NÃO INFORMADO" amount="--" date="NÃO INFORMADO" />
+      </>
     );
 
     expect(getByText('MXRF11')).toBeTruthy();
+    expect(getByText('VISC11')).toBeTruthy();
+    expect(getByText('RVBI11')).toBeTruthy();
+    expect(getByText('CPTS11')).toBeTruthy();
+    expect(getByText('MCHY11')).toBeTruthy();
   });
 
   it('displays alert', () => {
@@ -28,8 +38,8 @@ describe('InvestmentCard', () => {
     const botao_mais_detalhes = getByText("MXRF11")
 
     //presiona o botão
-    fireEvent(getByText("Rendimento: R$ 0,09"), 'click')
+    fireEvent(getByText("Mais detalhes"), 'click')
 
     expect(Alert.alert).toHaveBeenCalled();
-  });
+  });  
 });
